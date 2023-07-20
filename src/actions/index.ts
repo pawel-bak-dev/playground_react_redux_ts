8b3a7775020a4e55f8e4314e0b890bb17d641c85
pawel-bak-dev <pawel.bak.dev@gmail.com>
2023-07-20 23:16:31 +0200

    add actions

---
+++ src/actions/index.ts
@@ -0,0 +1,2 @@
+export * from 'actions/todos.ts';
+export * from 'actions/types.ts';
---
+++ src/actions/types.ts
@@ -0,0 +1,7 @@
+import { FetchTodosAction, DeleteTodoAction } from 'actions';
+export enum ActionTypes {
+  fetchTodos,
+  deleteTodo,
+}
+
+export type Action = FetchTodosAction | DeleteTodoAction;
